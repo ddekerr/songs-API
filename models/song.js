c861a17e10563473f8a1426445f25462f5a4a430
deker <dimon.bond.94@gmail.com>
2023-06-02 11:24:44 +0300

    Change songMongooseSchems

--- models/song.js
+++ models/song.js
@@ -9,7 +9,7 @@ const songMongooseSchema = new Schema(
     user_id: { type: String, required: true },
     created_by: { type: String, require: true },
     title: { type: String, required: true },
-    author: { type: String, required: true },
+    author: { type: String },
     genres: { type: GENRES, default: [] },
     path_to_song: String,
     path_to_video: String,
@@ -38,8 +38,20 @@ songMongooseSchema.post("save", handleMongooseError);
 
 const songJoiSchema = Joi.object({
   user_id: Joi.string().required(),
+  created_by: Joi.string().required(),
   title: Joi.string().required(),
-  author: Joi.string().required(),
+  author: Joi.string(),
+  genres: Joi.array().items(Joi.string().valid(...GENRES)),
+  path_to_song: Joi.string(),
+  path_to_video: Joi.string(),
+  path_to_img: Joi.string(),
+  text: Joi.array()
+    .items(Joi.object({ block_title: Joi.string(), block_text: Joi.string() }))
+    .required(),
+  chords: Joi.array().items(Joi.string()),
+  dowloaded: Joi.number(),
+  watched: Joi.number(),
+  listened: Joi.number(),
 });
 
 const Song = model("song", songMongooseSchema);
