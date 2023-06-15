eb6bba91e5e509114b4d1ead6fe31da05ca05558
deker <dimon.bond.94@gmail.com>
2023-06-15 15:09:12 +0300

    add update middleware

---
+++ middlewares/upload.js
@@ -0,0 +1,10 @@
+const path = require("path");
+const multer = require("multer");
+
+const tempDir = path.join(__dirname, "../", "temp");
+
+const multerConfig = multer.diskStorage({ destination: tempDir });
+
+const upload = multer({ storage: multerConfig });
+
+module.exports = upload;
